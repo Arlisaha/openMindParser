dd60568a1d3e151400a20a670096784c1c454705
Yann Di Serio <diserio.yann@gmail.com>
2016-11-24 15:10:53 +0000

    Tests/usage for the Generic HTML converter.

---
+++ tests/TestGenericHTMLConverter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace openMindParser\Tests;
+
+require __DIR__.'/../vendor/autoload.php';
+
+use openMindParser\Parser;
+use openMindParser\Converters\HTML\GenericHTMLConverter;
+
+$filePath = __DIR__.'/TestFile/Conduite_agressive.mm';
+
+$htmlConverter = GenericHTMLConverter::getInstance();
+$str = $htmlConverter->convert($filePath, [['tag' => 'ul', 'attributes' =>[]], ['tag' => 'li', 'attributes' =>[]]]);
+
+echo'<pre>';
+var_dump($str->saveHTML());
+die;
