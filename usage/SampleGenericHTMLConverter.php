99d609d89159012d55c2d074d4a20e2f51694dd7
Yann Di Serio <diserio.yann@gmail.com>
2016-11-25 11:10:23 +0000

    Change usage namespace.

--- usage/SampleGenericHTMLConverter.php
+++ usage/SampleGenericHTMLConverter.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace openMindParser\Tests\Usage;
+namespace openMindParser\Usage;
 
-require __DIR__.'/../../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 use openMindParser\Parser;
 use openMindParser\Converters\HTML\GenericHTMLConverter;
 
-$filePath = __DIR__.'/../TestFile/Conduite_agressive.mm';
+$filePath = __DIR__.'/../tests/TestFile/Conduite_agressive.mm';
 
 $htmlConverter = GenericHTMLConverter::getInstance();
 $str = $htmlConverter->convert($filePath, [['tag' => 'ul', 'attributes' =>[]], ['tag' => 'li', 'attributes' =>[]]]);
--- usage/SampleGenericJSONConverter.php
+++ usage/SampleGenericJSONConverter.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace openMindParser\Tests\Usage;
+namespace openMindParser\Usage;
 
-require __DIR__.'/../../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 use openMindParser\Parser;
 use openMindParser\Converters\JSON\GenericJSONConverter;
 
-$filePath = __DIR__.'/../TestFile/Conduite_agressive.mm';
+$filePath = __DIR__.'/../tests/TestFile/Conduite_agressive.mm';
 
 $htmlConverter = GenericJSONConverter::getInstance();
 $str = $htmlConverter->convert($filePath);
