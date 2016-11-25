90540aa0d72f7b80ac1d7a9d05b28b56aa9241dd
Yann Di Serio <diserio.yann@gmail.com>
2016-11-25 11:02:56 +0000

    Add comments.

--- src/Converters/JSON/GenericJSONConverter.php
+++ src/Converters/JSON/GenericJSONConverter.php
@@ -11,6 +11,14 @@ use \DOMDocument;
 /*A singleton to convert a document tree (object Document) in a JSON tree following few options.*/
 class GenericJSONConverter extends AbstractConverter
 {
+	/**
+	 * Perform the ocnversion in JSON by exporting the objects tree in an array tree and converting it in JSON.
+	 * 
+	 * @param Document $document : The Document instance to write in JSON.
+	 * @param array $options : The options to perform the conversion.
+	 * 
+	 * @return String : The JSON string.
+	 */
 	protected function convertFromDocumentInstance(Document $document, array $options) {
 		return json_encode($document->toArray());
 	}
