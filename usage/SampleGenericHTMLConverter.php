7486058b9597c938fcef57b171c2f36a04f2dc39
Yann Di Serio <diserio.yann@gmail.com>
2016-11-28 14:42:42 +0000

    Fix.

--- usage/SampleGenericHTMLConverter.php
+++ usage/SampleGenericHTMLConverter.php
@@ -10,7 +10,13 @@ use OpenMindParser\Converters\HTML\GenericHTMLConverter;
 $filePath = __DIR__.'/../tests/TestFile/Conduite_agressive.mm';
 
 $htmlConverter = GenericHTMLConverter::getInstance();
-$str = $htmlConverter->convert($filePath, [['tag' => 'ul', 'attributes' =>[]], ['tag' => 'li', 'attributes' =>[]]]);
+$str = $htmlConverter->convert($filePath, [
+	HTML_CONVERTER_MAIN_TAG_KEY => [
+		[HTML_CONVERTER_TAG_KEY => 'ul', HTML_CONVERTER_ATTRIBUTES_KEY =>[]], 
+		[HTML_CONVERTER_TAG_KEY => 'li', HTML_CONVERTER_ATTRIBUTES_KEY =>[]],
+	], 
+	HTML_CONVERTER_MAIN_ICON_KEY => true,
+]);
 
 echo'<pre>';
 var_dump($str->saveHTML());
