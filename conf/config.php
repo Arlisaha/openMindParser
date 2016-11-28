95712e5f711d5dc2374d3eb5aeef574d8a80a48d
Yann Di Serio <diserio.yann@gmail.com>
2016-11-28 14:17:18 +0000

    Add a config file and load it in composer.

---
+++ conf/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('PROJECT_ROOT_DIR', realpath(__DIR__.'/..'));
+define('IMG_FULL_ROOT_DIR', PROJECT_ROOT_DIR.'/img/');
+define('IMG_ROOT_DIR', '/'.basename(PROJECT_ROOT_DIR).'/img/');
+define('SRC_FULL_ROOT_DIR', PROJECT_ROOT_DIR.'/src/');
+define('SRC_ROOT_DIR', '/'.basename(PROJECT_ROOT_DIR).'/src/');
