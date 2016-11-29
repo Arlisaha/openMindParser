69ea1b4e4f15d9e4833d3541b913e34302d5b0fe
Yann Di Serio <diserio.yann@gmail.com>
2016-11-29 09:19:36 +0000

    Tests for converters.

--- tests/Converters/GenericHTMLConverterTest.php
+++ tests/Converters/GenericHTMLConverterTest.php
@@ -3,7 +3,26 @@
 namespace openMindParser\Tests\Converters;
 
 use \PHPUnit_Framework_TestCase as TestCase;
+use OpenMindParser\Converters\HTML\GenericHTMLConverter;
 
 class GenericHTMLConverterTest extends TestCase
 {
+	public function testConvert() {
+		$filePath = TESTS_FULL_ROOT_DIR.'TestFile/test_sample.mm';
+		$awaitedHtml = '<ul><li style="color:#000000;font-family:SansSerif;font-size:20;" id="0">node_0<ul><li style="color:#010101;font-family:SansSerif;font-size:18;" id="0_1">node_0_1<ul><li style="color:#011011;" id="0_1_1">node_0_1_1</li></ul></li></ul><ul><li style="color:#020202;font-family:SansSerif;font-size:16;" id="0_2"><img src="/openMindParser/img/button_ok.png" alt="button_ok">node_0_2</li></ul></li></ul>';
+		$domDocument = new \DOMDocument();
+		$domDocument->loadHTML($awaitedHtml, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+		
+		$htmlConverter = GenericHTMLConverter::getInstance();
+		
+		$htmlStr = $htmlConverter->convert($filePath, [
+			HTML_CONVERTER_MAIN_TAG_KEY => [
+				[HTML_CONVERTER_TAG_KEY => 'ul', HTML_CONVERTER_ATTRIBUTES_KEY =>[]], 
+				[HTML_CONVERTER_TAG_KEY => 'li', HTML_CONVERTER_ATTRIBUTES_KEY =>[]],
+			], 
+			HTML_CONVERTER_MAIN_ICON_KEY => true,
+		]);
+		
+		$this->assertEquals($domDocument->saveHTML(), $htmlStr->saveHTML());
+	}
 }
--- tests/Converters/GenericJSONConverterTest.php
+++ tests/Converters/GenericJSONConverterTest.php
@@ -3,7 +3,81 @@
 namespace openMindParser\Tests\Converters;
 
 use \PHPUnit_Framework_TestCase as TestCase;
+use OpenMindParser\Converters\JSON\GenericJSONConverter;
 
 class GenericJSONConverterTest extends TestCase
 {
+	public function testConvert() {
+		$filePath = TESTS_FULL_ROOT_DIR.'TestFile/test_sample.mm';
+		$awaitedJson = json_encode(
+			[
+			'path'     => $filePath,
+			'name'     => basename($filePath),
+			'size'     => filesize($filePath),
+			'rootNode' => [
+				'id'       => '0',
+				'color'    => '#000000', 
+				'created'  => '0000000000000',
+				'modified' => '0000000000000',
+				'position' => null,
+				'vshift'   => null,
+				'folded'   => null,
+				'text'     => 'node_0',
+				'fontName' => 'SansSerif',
+				'fontSize' => '20',
+				'icon'     => null,
+				'children' => [
+					[	
+						'id'       => '0_1',
+						'color'    => '#010101', 
+						'created'  => '0000000000000',
+						'modified' => '0000000000000',
+						'position' => 'right',
+						'vshift'   => '-45',
+						'folded'   => 'true',
+						'text'     => 'node_0_1',
+						'fontName' => 'SansSerif',
+						'fontSize' => '18',
+						'icon'     => null,
+						'children' => [
+							[
+								'id'       => '0_1_1',
+								'color'    => '#011011', 
+								'created'  => '0000000000000',
+								'modified' => '0000000000000',
+								'position' => null,
+								'vshift'   => null,
+								'folded'   => null,
+								'text'     => 'node_0_1_1',
+								'fontName' => null,
+								'fontSize' => null,
+								'icon'     => null,
+								'children' => [],
+							],
+						],
+					],
+					[
+						'id'       => '0_2',
+						'color'    => '#020202', 
+						'created'  => '0000000000000',
+						'modified' => '0000000000000',
+						'position' => 'right',
+						'vshift'   => '-45',
+						'folded'   => 'true',
+						'text'     => 'node_0_2',
+						'fontName' => 'SansSerif',
+						'fontSize' => '16',
+						'icon'     => IMG_ROOT_DIR.'button_ok.png',
+						'children' => [],
+					],
+				],
+			],
+		]
+		);
+		$jsonConverter = GenericJSONConverter::getInstance();
+		
+		$jsonStr = $jsonConverter->convert($filePath);
+		
+		$this->assertEquals($awaitedJson, $jsonStr);
+	}
 }
