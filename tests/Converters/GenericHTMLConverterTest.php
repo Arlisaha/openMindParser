96e61ad40ddd3b260f3602d29d59cd2b60a65f02
Yann Di Serio <diserio.yann@gmail.com>
2016-11-29 10:05:46 +0000

    Fix test again.

--- tests/Converters/GenericHTMLConverterTest.php
+++ tests/Converters/GenericHTMLConverterTest.php
@@ -10,8 +10,6 @@ class GenericHTMLConverterTest extends TestCase
 	public function testConvert() {
 		$filePath = TESTS_FULL_ROOT_DIR.'TestFile/test_sample.mm';
 		$awaitedHtml = '<ul><li style="color:#000000;font-family:SansSerif;font-size:20;" id="0">node_0<ul><li style="color:#010101;font-family:SansSerif;font-size:18;" id="0_1">node_0_1<ul><li style="color:#011011;" id="0_1_1">node_0_1_1</li></ul></li></ul><ul><li style="color:#020202;font-family:SansSerif;font-size:16;" id="0_2"><img src="/openMindParser/img/button_ok.png" alt="button_ok">node_0_2</li></ul></li></ul>';
-		$domDocument = new \DOMDocument();
-		$domDocument->loadHTML($awaitedHtml, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
 		
 		$htmlConverter = GenericHTMLConverter::getInstance();
 		
@@ -23,6 +21,6 @@ class GenericHTMLConverterTest extends TestCase
 			HTML_CONVERTER_MAIN_ICON_KEY => true,
 		]);
 		
-		$this->assertEquals(trim($domDocument->saveHTML()), trim($htmlStr->saveHTML()));
+		$this->assertEquals(trim($awaitedHtml), trim($htmlStr->saveHTML()));
 	}
 }
